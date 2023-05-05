601af16e8fdf637a669a6c2ed0c27438613669b8
Daniel Herkert <daniel.herkert@gmail.com>
2023-05-05 11:02:28 +0300

    Live blobs working

--- Examples/LiveBlobs.gd
+++ Examples/LiveBlobs.gd
@@ -3,6 +3,12 @@
 extends Node3D
 
 
+@export var refresh_blobs = false:
+	set(nv):
+		for vox_node in get_children():
+			vox_node.apply_paintstack()
+
+
 @export var paint_stack : VoxelPaintStack
 
 # Called when the node enters the scene tree for the first time.
@@ -29,8 +35,11 @@ func _process(delta):
 	if frame_limit_counter >= frame_limit:
 		frame_limit_counter -= frame_limit
 		var phase = (sin(elapsed/2.0)+1.0)/2.0
-		paint_stack.operation_stack[0].plane.y = phase
-		#paint_stack.operation_stack[1].high = phase * 32.0
+		var phase2 = (sin(elapsed/3.387)+1.0)/3.387
+		var phase3 = (sin(elapsed/4.561)+1.0)/4.561
+		paint_stack.operation_stack[1].center.y = phase * 32.0
+		paint_stack.operation_stack[2].center.y = phase2 * 32.0
+		paint_stack.operation_stack[3].center.y = phase3 * 32.0
 		#print(paint_stack.operation_stack[1].high)
 		#for vox_node in get_children():
 		#	vox_node.apply_paintstack()
