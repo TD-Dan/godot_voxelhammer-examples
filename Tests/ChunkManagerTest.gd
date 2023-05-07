f2f6d41251492b4f260959ab99f46f9e0f91da1d
Daniel Herkert <daniel.herkert@gmail.com>
2023-05-07 16:44:39 +0300

    Added persistent Chunk loading

--- Tests/ChunkManagerTest.gd
+++ Tests/ChunkManagerTest.gd
@@ -34,25 +34,28 @@ func _ready():
 
 func _on_chunk_created(chunk):
 	print("Received 'new_chunk_created' with %s" % chunk)
-	var new_debug_mesh = DebugMesh.new(Color(1,0.5,0))
-	new_debug_mesh.position = chunk.position
-	new_debug_mesh.scale = Vector3i(chunkmanager.chunk_size,chunkmanager.chunk_size,chunkmanager.chunk_size)
-	chunk.data["DebugMesh"] = new_debug_mesh
-	add_child(new_debug_mesh)
+	chunk.persistent_data["BytesData"] = PackedByteArray()
+	chunk.persistent_data["BytesData"].resize(64*64*64)
+	chunk.persistent_data["BytesData"].fill(2)
 
 
 func _on_chunk_loaded(chunk):
 	print("Received 'chunk_loaded' with %s" % chunk)
-	chunk.data["DebugMesh"].color = Color(0.1,0.1,0.1)
+	var new_debug_mesh = DebugMesh.new(Color(1,0.5,0))
+	new_debug_mesh.position = chunk.position
+	new_debug_mesh.scale = Vector3i(chunkmanager.chunk_size,chunkmanager.chunk_size,chunkmanager.chunk_size)
+	chunk.transient_data["DebugMesh"] = new_debug_mesh
+	chunk.transient_data["DebugMesh"].color = Color(0.1,0.1,0.1)
+	add_child(new_debug_mesh)
 
 
 func _on_chunk_activated(chunk):
 	print("Received 'chunk_activated' with %s" % chunk)
-	chunk.data["DebugMesh"].color = Color(0.1,1.0,0.1)
+	chunk.transient_data["DebugMesh"].color = Color(0.1,1.0,0.1)
 
 
 func get_status_text():
-	return "CHUNKSERVER: %s chunks, %s active\n%s hotspots" % [chunkmanager.chunks.size(), chunkmanager.active_chunks, chunkmanager.hotspots.size()]
+	return "CHUNKSERVER: %s chunks, %s active\n%s hotspots" % [chunkmanager.chunks.size(), chunkmanager.active_chunks.size(), chunkmanager.hotspots.size()]
 
 
 func _on_update_timer_timeout():
