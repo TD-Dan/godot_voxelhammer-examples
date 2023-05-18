fecf7dcd9d96fe640f9d7be227680851a27186b8
Daniel Herkert <daniel.herkert@gmail.com>
2023-05-18 21:47:54 +0300

    pretty colors

--- Tests/ChunkManagerTest.gd
+++ Tests/ChunkManagerTest.gd
@@ -52,7 +52,7 @@ func _on_chunk_initialized(chunk : Chunk):
 	if chunk.transient_data.has("DebugMesh"):
 		push_error('ChunkMnagerTest: Found existing chunk.transient_data["DebugMesh"]!')
 		
-	var new_debug_mesh = DebugMesh.new(Color(0., 0., 0., .3))
+	var new_debug_mesh = DebugMesh.new(Color(.5, .5, .5, .2))
 	
 	new_debug_mesh.position = chunk.position
 	new_debug_mesh.scale = Vector3i(chunkmanager.chunk_size,chunkmanager.chunk_size,chunkmanager.chunk_size)
@@ -71,7 +71,7 @@ func _on_chunk_created(chunk: Chunk):
 		chunk.transient_data["DebugMesh"] = new_debug_mesh
 		add_child(new_debug_mesh)
 	
-	chunk.transient_data["DebugMesh"].color = Color(0., 1., 1., .8)
+	chunk.transient_data["DebugMesh"].color = Color(.5, .5, 1., .8)
 	chunk.transient_data["DebugMesh"].text += "created at %s\n" % Time.get_datetime_string_from_system()
 	
 	chunk.persistent_data["BytesData"] = PackedByteArray()
@@ -92,7 +92,7 @@ func _on_chunk_loaded(chunk : Chunk):
 		chunk.transient_data["DebugMesh"] = new_debug_mesh
 		add_child(new_debug_mesh)
 		
-	chunk.transient_data["DebugMesh"].color = Color(.1, .1, .1, .8)
+	chunk.transient_data["DebugMesh"].color = Color(.4, .4, .8, .8)
 	chunk.transient_data["DebugMesh"].text += "loaded at %s\n" % Time.get_datetime_string_from_system()
 
 
@@ -108,14 +108,18 @@ func _on_chunk_activated(chunk : Chunk):
 	
 	
 	chunk.transient_data["CounterLabel"] = Label3D.new()
-	chunk.transient_data["CounterLabel"].text = "%s" % chunk.persistent_data["Counter"]
+	chunk.transient_data["CounterLabel"].text = "saved value:%s" % chunk.persistent_data["Counter"]
+	chunk.transient_data["CounterLabel"].pixel_size = 0.001
+	chunk.transient_data["CounterLabel"].modulate = chunk.transient_data["DebugMesh"].color
+	chunk.transient_data["CounterLabel"].position.y = 0.5
 	chunk.transient_data["CounterLabel"].vertical_alignment = VERTICAL_ALIGNMENT_BOTTOM
+	chunk.transient_data["CounterLabel"].horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
 	
 	chunk.transient_data["DebugMesh"].add_child(chunk.transient_data["CounterLabel"])
 
 func _on_chunk_deactivated(chunk : Chunk):
 	#print("Received 'chunk_deactivated' with %s" % chunk)
-	chunk.transient_data["DebugMesh"].color = Color(.1, .1, .2, .8)
+	chunk.transient_data["DebugMesh"].color = Color(.4, .4, .8, .8)
 	chunk.transient_data["DebugMesh"].text += "Deactivated at %s \n" % Time.get_datetime_string_from_system()
 	chunk.transient_data["CounterLabel"].queue_free()
 	chunk.transient_data.erase("CounterLabel")
@@ -129,7 +133,7 @@ func _on_chunk_unloaded(chunk : Chunk):
 
 func _on_chunk_deleted(chunk : Chunk):
 	#print("Received 'chunk_unloaded' with %s" % chunk)
-	chunk.transient_data["DebugMesh"].color = Color(1,0,0)
+	chunk.transient_data["DebugMesh"].color = Color(1., 0., 0., .5)
 	remove_child(chunk.transient_data["DebugMesh"])
 	chunk.transient_data["DebugMesh"].queue_free()
 
@@ -141,6 +145,10 @@ func _on_shortest_distance_updated(chunk : Chunk):
 	if not chunk.transient_data.has("DistanceLabel"):
 		chunk.transient_data["DistanceLabel"] = Label3D.new()
 		chunk.transient_data["DistanceLabel"].vertical_alignment = VERTICAL_ALIGNMENT_TOP
+		chunk.transient_data["DistanceLabel"].horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+		chunk.transient_data["DistanceLabel"].pixel_size = 0.001
+		chunk.transient_data["DistanceLabel"].position.y = 0.5
+		chunk.transient_data["DistanceLabel"].modulate = chunk.transient_data["DebugMesh"].color
 		chunk.transient_data["DebugMesh"].add_child(chunk.transient_data["DistanceLabel"])
 	chunk.transient_data["DistanceLabel"].text = "%d.1" % chunk.dist_to_closest_hotspot
 
