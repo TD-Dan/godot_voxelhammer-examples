18676f74ea9f24646f794ba06cf98ac92545712e
Daniel Herkert <daniel.herkert@gmail.com>
2024-03-29 17:48:25 +0200

    Cleared errors from test 11

---
+++ Tests/parts/ChunkManagerTest.gd
@@ -0,0 +1,40 @@
+@tool
+
+extends Node
+
+@export var chunk_space : ChunkSpace3D
+
+@export var add_hotspot : NodePath:
+	set(nv):
+		chunk_space.add_hotspot(get_node_or_null(nv))
+
+@export var remove_hotspot : NodePath:
+	set(nv):
+		chunk_space.remove_hotspot(get_node_or_null(nv))
+
+@export var print_status : bool = false:
+	set(_nv):
+		print(get_status_text())
+
+
+func _ready():
+	
+	chunk_space.add_hotspot(%TestTarget)
+	chunk_space.add_hotspot(%TestTarget2)
+	chunk_space.add_hotspot(%MovingTarget)
+	chunk_space.add_hotspot(%MovingTarget2)
+
+
+func _process(delta):
+	%MovingTarget.progress_ratio += delta/200.0
+	%MovingTarget2.progress_ratio += delta/50.0
+
+	%StatusLabel.text = get_status_text()
+
+func get_status_text():
+	return "ChunkSpace3D: %s chunks \n%s hotspots" % \
+		[chunk_space.chunks_by_position.size(), chunk_space._hotspots.size()]
+
+
+func _on_update_timer_timeout():
+	%StatusLabel.text = get_status_text()
